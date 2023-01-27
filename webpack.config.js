fe7c267f9776e5a5090b086555c589bb38860c66
Jack Holloway <jack@mrprimate.co.uk>
2023-01-27 21:36:02 +0000

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   devtool: "source-map",
-  entry: './src/index.js',
+  entry: './src/module.js',
   optimization: {
     minimize: false,
     minimizer: [
