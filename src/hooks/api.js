cefec52470c68d32954d9e73a28fee1f29752c83
Jack Holloway <jack@mrprimate.co.uk>
2023-01-30 19:48:14 +0000

    Remove PredicatePF2e stub

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,14 +1,12 @@
 import { Pathmuncher } from "../app/Pathmuncher.js";
 import { PathmuncherImporter } from "../app/PathmuncherImporter.js";
 import CONSTANTS from "../constants.js";
-import { PredicatePF2e } from "../lib/PredicatePF2e.js";
 import { EQUIPMENT_RENAME_MAP, RESTRICTED_EQUIPMENT } from "../data/equipment.js";
 import { FEAT_RENAME_MAP } from "../data/features.js";
 import utils from "../utils.js";
 
 export function registerAPI() {
   game.modules.get(CONSTANTS.MODULE_NAME).api = {
-    PredicateMuncher: PredicatePF2e,
     Pathmuncher,
     PathmuncherImporter,
     data: {
